b8be4620ed4a89ee1c1faf210a82f0b68aae5107
Luciana Lima <lucianalima.lml@gmail.com>
2017-02-12 22:35:23 -0200

    Menu side of content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  sideNavOpened = false;
 
+  toggle(){
+    this.sideNavOpened = ! this.sideNavOpened;
+  }
 }

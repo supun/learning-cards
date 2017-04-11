9782dee79e601e9a12756ee9dc3a2ff70a331e0b
supun921@gmail.com <supun921@gmail.com>
2017-04-11 18:10:40 +0530

    layout issues fixed

--- src/app/shared/lesson.service.ts
+++ src/app/shared/lesson.service.ts
@@ -35,8 +35,8 @@ export class LessonService {
   }
 
   getLessionsData():Observable<Lesson[]> {
-    return this.http.get('http://mobile-first.me/wp-json/acf/v3/posts')
-  // return this.http.get('lessons.json')
+//    return this.http.get('http://mobile-first.me/wp-json/acf/v3/posts')
+   return this.http.get('lessons.json')
                   .do(console.log)
 //                  .map(res => res.json())
             .map(res => res.json().lessons)
